61ae459ff5cee3793051addf294fc63767ffa7b6
S€th <143244075+wont-stream@users.noreply.github.com>
2024-12-09 15:40:01 -0500

    Overhall

--- openOptimizer/index.ts
+++ openOptimizer/index.ts
@@ -18,35 +18,32 @@
 
 import definePlugin from "@utils/types";
 
-let _removeChild: <T extends Node>(child: T) => T;
-let _appendChild: <T extends Node>(child: T) => T;
-
-
-const optimize = orig => function (...args) {
-    if (typeof args[0].className === 'string' && (args[0].className.indexOf('activity') !== -1 ||
-        args[0].className.indexOf('subText') !== -1 ||
-        args[0].className.indexOf('botText') !== -1 ||
-        args[0].className.indexOf('clanTag') !== -1))
-        // @ts-ignore
-        return setTimeout(() => orig.apply(this, args), 100);
-
-    // @ts-ignore
-    return orig.apply(this, args);
-};
-
 export default definePlugin({
     name: "Open Optimizer",
     description: "Ports OpenAsar's optimizer code.",
     authors: [{ name: "S€th", id: 1273447359417942128n }],
-    start: () => {
-        _removeChild = Element.prototype.removeChild;
-        _appendChild = Element.prototype.appendChild;
-
-        Element.prototype.removeChild = optimize(Element.prototype.removeChild);
-        Element.prototype.appendChild = optimize(Element.prototype.appendChild);
+    methods: ['removeChild', 'appendChild'],
+    start() {
+        for (const method of this.methods) {
+            this[`_${method}`] = Element.prototype[method];
+            Element.prototype[method] = this.optimize(Element.prototype[method]);
+        }
     },
-    stop: () => {
-        Element.prototype.removeChild = _removeChild;
-        Element.prototype.appendChild = _appendChild;
+    stop() {
+        for (const method of this.methods) {
+            Element.prototype[method] = this[`_${method}`];
+        }
+    },
+
+    optimize: orig => function (...args) {
+        if (typeof args[0].className === 'string' && (args[0].className.indexOf('activity') !== -1 ||
+            args[0].className.indexOf('subText') !== -1 ||
+            args[0].className.indexOf('botText') !== -1 ||
+            args[0].className.indexOf('clanTag') !== -1))
+            // @ts-ignore
+            return setTimeout(() => orig.apply(this, args), 100);
+    
+        // @ts-ignore
+        return orig.apply(this, args);
     }
 });
